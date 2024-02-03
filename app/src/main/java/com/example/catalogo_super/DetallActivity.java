57c55b49db3c770d1a16245c21fdbc9027c7de11
jesus sanchez sarmiento <xhuxho.emprende@gmail.com>
2024-02-03 01:07:00 -0600

    pasar datos por extras y despues colocarlos en pantalla

--- app/src/main/java/com/example/catalogo_super/DetallActivity.java
+++ app/src/main/java/com/example/catalogo_super/DetallActivity.java
@@ -4,11 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.catalogo_super.databinding.ActivityDetallBinding;
+import com.example.catalogo_super.databinding.ActivityMainBinding;
+
 public class DetallActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detall);
+        ActivityDetallBinding binding = ActivityDetallBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // creamos una variable con multiples valores como un json
+        Bundle extras = getIntent().getExtras();
+        // tomamos los valores y los ponemos en variables locales
+        String superheroName = extras.getString("superhero_name");
+        String alterEgo = extras.getString("alter_ego");
+        String bio = extras.getString("bio");
+        float rating = extras.getFloat("rating");
+
+        binding.heroName.setText(superheroName);
+        binding.alterEgoText.setText(alterEgo);
+        binding.bioText.setText(bio);
+        binding.ratingBar.setRating(rating);
+
     }
 }
--- app/src/main/java/com/example/catalogo_super/MainActivity.java
+++ app/src/main/java/com/example/catalogo_super/MainActivity.java
@@ -16,11 +16,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         binding.saveButton.setOnClickListener(v -> {
-            openDetailActivity();
+            String superheroName = binding.heroNameEdit.getText().toString();
+            String alterEgo = binding.alterEgoEdit.getText().toString();
+            String bio = binding.bioEdit.getText().toString();
+            float rating = binding.powerBar.getRating();
+            openDetailActivity(superheroName,alterEgo,bio,rating);
         });
     }
-    private void openDetailActivity(){
+    private void openDetailActivity(String superheroName,String alterEgo,String bio,float rating){
         Intent intent = new Intent(this,DetallActivity.class);
+        intent.putExtra("superhero_name", superheroName);
+        intent.putExtra("alter_ego", alterEgo);
+        intent.putExtra("bio", bio);
+        intent.putExtra("rating", rating);
         startActivity(intent);
     }
 }
